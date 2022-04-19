49fcdb1ad0a0b555d7f029605ba252bfd7c62647
tanluong0503 <nguyentanluong0503@gmail.com>
2022-04-19 14:31:54 +0700

    hcn.html

---
+++ hcn.js
@@ -0,0 +1,9 @@
+
+let inputWidth;
+let inputHeight;
+ inputWidth = prompt("Enter the Width");
+ inputHeight = prompt("Enter the Height");
+let Width = parseInt(inputWidth);
+let Height = parseInt(inputHeight);
+let area = Width*Height;
+document.write("The area is: "+ area); 
