34d0084a89344e07ee2177dc2b1a9b4e41618993
tanluong0503 <nguyentanluong0503@gmail.com>
2022-04-26 17:05:18 +0700

    Oto.html

---
+++ Oto.js
@@ -0,0 +1,105 @@
+var products = [
+   "Lamborghini",
+    "Ferrari",
+    "Mercedes",
+    "BMW",
+    "Rolls-Royce",
+    "MG",
+    "Maybach",
+    "VinFast",
+    "Toyota",
+    "Bugatti",
+    "Honda"
+]
+
+const key_enter = 13;
+// var position = 0;
+
+function renderProduct() {
+    let tbProducts = document.querySelector('.table>tbody');
+    let htmls = products.map(function (product, index) {
+        return `
+                <tr>
+                    <td>${index + 1}</td>
+                    <td>${product}</td>
+                    <td>
+                        <button class="btn" onclick="modify(${index})">Edit</button>
+                        <button class="btn" onclick='removeProduct(${index})'>Remove</button>
+                    </td>
+                </tr>
+                `
+    })
+    tbProducts.innerHTML = htmls.join("");
+}
+
+function createProduct() {
+    // 1. lấy được tên sản phẩm muốn thêm vào
+    // 2. Bổ sung sản phẩm này vào danh sách products
+    // 2.1 tên phẩm phải khác null hoặc rõng mới được thêm vào
+    // 2.2 ngược lại thì hiển thị thông báo
+    // 3. Hiển thị lại dữ liệu
+    // 4. xóa input chưa tên sản phẩm và đặt focus
+    let productName = document.querySelector("#productName").value;
+    if (productName != null && productName.trim() != "") {
+        products.push(productName);
+        reset();
+        renderProduct();
+    }
+    else {
+        alert("Product name is required!")
+        document.querySelector("#productName").focus();
+    }
+}
+
+function reset() {
+    document.querySelector("#productName").value = "";
+}
+
+function pressEnter(e) {
+    if (e.keyCode == key_enter) {
+        createProduct();
+    }
+}
+
+function removeProduct(index) {
+    let confirmed = window.confirm(`Are sure to want to remove ${products[index]}?`);
+    if (confirmed) {
+        products.splice(index, 1);
+        renderProduct();
+    }
+}
+
+function modify(index) {
+    document.querySelector('#modifyProduct').classList.remove('d-none');
+    document.querySelector("#new_ProductName").value = products[index];
+    // position = index;
+    document.querySelector("#btnUpdate").onclick = function () {
+        let new_ProductName = document.querySelector("#new_ProductName").value;
+        if (new_ProductName != null && new_ProductName.trim() != "") {
+            products[index] = new_ProductName;
+            renderProduct();
+            clearForm();
+        }
+        else {
+            alert("Product name is required!")
+            document.querySelector("#new_ProductName").focus();
+        }
+    }
+}
+
+function clearForm() {
+    document.querySelector('#modifyProduct').classList.add('d-none');
+    position = 0;
+}
+
+
+function init() {
+    // document.querySelector("#productName").onkeydown = function(e){
+    //     if(e.keyCode == key_enter){
+    //         createProduct();
+    //     }
+    // }
+    renderProduct();
+}
+
+init();
